5259f15d35f0d86ad86919e1d5588318c0023bf5
Nitesh <05niteshraj@gmail.com>
2023-04-29 01:04:21 +0530

    Added createTweetWithEmail and getMyTweets functions and modify other functions;

--- backend/src/controller/tweet.controller.js
+++ backend/src/controller/tweet.controller.js
@@ -1,6 +1,5 @@
 import database from "../config/mysql.config.js";
 import HttpStatus from "../domain/httpStatus.js";
-import Response from "../domain/response.js";
 import QUERY from "../query/tweetdb.query.js";
 import logger from "../util/logger.js";
 
@@ -8,26 +7,21 @@ export const getTweets = (req, res) => {
   logger.info(`${req.method}:${req.originalUrl}, Getting the tweet...`);
   database.query(QUERY.TWEET.SELECT_ALL, (error, results) => {
     if (!results) {
-      res
-        .status(HttpStatus.OK.code)
-        .send(
-          new Response(
-            HttpStatus.OK.code,
-            HttpStatus.OK.status,
-            "No tweet found"
-          )
-        );
+      res.send({
+        status: HttpStatus.NO_CONTENT.code,
+        message: "No tweet found",
+      });
+    } else if (error) {
+      res.send({
+        status: HttpStatus.INTERNAL_SERVER_ERROR.code,
+        message: "There is an Error",
+      });
     } else {
-      res.status(HttpStatus.OK.code).send(
-        new Response(
-          HttpStatus.OK.code,
-          HttpStatus.OK.status,
-          "Tweet:",
-          {
-            tweet: results,
-          }
-        )
-      );
+      res.send({
+        status: HttpStatus.OK.code,
+        message: "Tweets found",
+        tweets: results,
+      });
     }
   });
 };
@@ -36,56 +30,107 @@ export const getTweet = (req, res) => {
   logger.info(`${req.method}:${req.originalUrl}, Getting the tweet...`);
   database.query(QUERY.TWEET.SELECT, [req.params.id], (error, results) => {
     if (!results) {
-      res
-        .status(HttpStatus.OK.code)
-        .send(
-          new Response(
-            HttpStatus.OK.code,
-            HttpStatus.OK.status,
-            "No tweet found"
-          )
-        );
+      res.send({
+        status: HttpStatus.NO_CONTENT.code,
+        message: "No tweet found",
+      });
+    } else if (!results[0]) {
+      res.send({
+        status: HttpStatus.NO_CONTENT.code,
+        message: "No tweet found",
+      });
     } else {
-      res.status(HttpStatus.OK.code).send(
-        new Response(
-          HttpStatus.OK.code,
-          HttpStatus.OK.status,
-          "Tweets: ",
-          {
-            tweets: results,
-          }
-        )
-      );
+      res.send({
+        status: HttpStatus.OK.code,
+        message: "Tweet found",
+        tweet: results,
+      });
     }
   });
+};
+
+export const getMyTweets = (req, res) => {
+  logger.info(`${req.method}:${req.originalUrl}, Getting my tweets...`);
+  database.query(
+    QUERY.TWEET.SELECT_WITH_EMAIL,
+    req.body.email,
+    (error, results) => {
+      if (!results) {
+        res.send({
+          status: HttpStatus.NO_CONTENT.code,
+          message: "No tweet found",
+        });
+      } else if (error) {
+        res.send({
+          status: HttpStatus.INTERNAL_SERVER_ERROR.code,
+          message: "There is an Error",
+        });
+      } else {
+        res.send({
+          status: HttpStatus.OK.code,
+          message: "Tweets found",
+          tweets: results,
+        });
+      }
+    }
+  );
 };
 
 export const createTweet = (req, res) => {
   logger.info(`${req.method} : ${req.originalUrl} , creating tweet...`);
 
-  database.query(QUERY.TWEET.CREATE, Object.values(req.body), (error, results) => {
+  database.query(
+    QUERY.TWEET.CREATE,
+    Object.values(req.body),
+    (error, results) => {
+      if (error) {
+        res.send({
+          status: HttpStatus.INTERNAL_SERVER_ERROR.code,
+          message: "There is an Error",
+        });
+      } else {
+        res.send({
+          status: HttpStatus.CREATED.code,
+          message: "tweet Created",
+        });
+      }
+    }
+  );
+};
+
+export const createTweetWithEmail = (req, res) => {
+  logger.info(`${req.method} : ${req.originalUrl} , creating tweet...`);
+
+  // req.body = {content, email}
+  database.query(QUERY.USER.CHECK_EMAIL, req.body.email, (error, results) => {
     if (error) {
-      res
-        .status(HttpStatus.INTERNAL_SERVER_ERROR.code)
-        .send(
-          new Response(
-            HttpStatus.INTERNAL_SERVER_ERROR.code,
-            HttpStatus.INTERNAL_SERVER_ERROR.status,
-            "Server Problem",
-            { error: error }
-          )
-        );
+      res.send({
+        status: HttpStatus.INTERNAL_SERVER_ERROR.code,
+        message: "There is an error",
+      });
+    } else if (results[0]["COUNT(*)"] == "0") {
+      res.send({
+        status: HttpStatus.FORBIDDEN.code,
+        message: "Email does not exists",
+      });
     } else {
-      res
-        .status(HttpStatus.CREATED.code)
-        .send(
-          new Response(
-            HttpStatus.CREATED.code,
-            HttpStatus.CREATED.status,
-            "tweet Created",
-            { query_info: results }
-          )
-        );
+      database.query(
+        QUERY.TWEET.CREATE_WITH_EMAIL,
+        Object.values(req.body),
+        (error, results) => {
+          if (error) {
+            res.send({
+              status: HttpStatus.INTERNAL_SERVER_ERROR.code,
+              message: "There is an Error",
+            });
+          } else {
+            res.send({
+              status: HttpStatus.CREATED.code,
+              message: "tweet Created",
+            });
+          }
+        }
+      );
     }
   });
 };
