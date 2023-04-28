82e5f6b054bddc53fd1d539e159cbfe842f336ff
Nitesh <05niteshraj@gmail.com>
2023-04-29 01:05:20 +0530

    Edited as required;

--- backend/src/domain/httpStatus.js
+++ backend/src/domain/httpStatus.js
@@ -1,10 +1,12 @@
 const HttpStatus = {
-    OK: { code: 200, status: 'OK'},
-    CREATED: {code: 201, status: 'CREATED'},
-    NO_CONTENT: {code: 204, status: 'NO_CONTENT'},
-    BAD_REQUEST: {code: 400, status: 'BAD_REQUEST'},
-    NOT_FOUND: {code: 404, status: 'NOT_FOUND'},
-    INTERNAL_SERVER_ERROR: {code: 500, status: 'INTERNAL_SERVER_ERROR'}
-}
+  OK: { code: 200, status: "OK" },
+  CREATED: { code: 201, status: "CREATED" },
+  NO_CONTENT: { code: 204, status: "NO_CONTENT" },
+  BAD_REQUEST: { code: 400, status: "BAD_REQUEST" },
+  FORBIDDEN: { code: 403, status: "FORBIDDEN" },
+  NOT_FOUND: { code: 404, status: "NOT_FOUND" },
+  CONFLICT: { code: 409, status: "CONFICT" },
+  INTERNAL_SERVER_ERROR: { code: 500, status: "INTERNAL_SERVER_ERROR" },
+};
 
 export default HttpStatus;
