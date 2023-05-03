0f5ce05a28c528daed769f5927d7d23411d3f487
WinayGupta777 <vinay.gupta8101@gmail.com>
2023-05-03 11:33:33 +0530

    As user registered / signed in, the details will be displayed in ProfileBox component;

--- frontend/src/components/ProfileBox.js
+++ frontend/src/components/ProfileBox.js
@@ -1,10 +1,16 @@
 import { Settings } from "@mui/icons-material";
 import { Avatar, Box, IconButton, Typography } from "@mui/material";
+import { useSelector } from "react-redux";
 
 const ProfileBox = () => {
+
+    const currentUser = useSelector(
+        (state) => state.userReducer.userInfo
+    );
+
     return (
         <Box sx={{ mt: 1, mb: 2 }}>
-            <Box sx={{ pl: 1, pr: 2, display: 'flex', justifyContent: 'space-between'}}>
+            <Box sx={{ pl: 1, pr: 2, display: 'flex', justifyContent: 'space-between' }}>
                 <Typography sx={{ pl: 2, fontSize: 28 }}> ◉ Profile</Typography>
                 <IconButton> <Settings />  </IconButton>
             </Box>
@@ -13,8 +19,8 @@ const ProfileBox = () => {
                 justifyContent: 'center', mt: 5
             }}>
                 <Avatar sx={{ width: 220, height: 220, mb: 3 }} />
-                <Typography variant="h5">Dummy User</Typography>
-                <Typography >dummy@mock.com</Typography>
+                <Typography variant="h5"> {currentUser[0]} </Typography>
+                <Typography >{currentUser[1]} </Typography>
             </Box>
         </Box>
     );
