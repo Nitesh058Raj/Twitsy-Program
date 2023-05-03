69fdad15313970e1cfca5c5068bfe7a53337a14d
WinayGupta777 <vinay.gupta8101@gmail.com>
2023-05-03 10:36:54 +0530

    Logout function implemented;

--- frontend/src/components/ApplicationSettings.js
+++ frontend/src/components/ApplicationSettings.js
@@ -2,13 +2,22 @@ import React from 'react';
 import { useSelector, useDispatch } from 'react-redux';
 import { Box, Button, IconButton } from '@mui/material';
 import { BrightnessHigh, BrightnessLow } from '@mui/icons-material';
-import { setGlobalTheme } from "../redux/theme/themeAction"
+import { setGlobalTheme } from "../redux/theme/themeAction";
+import { useNavigate } from "react-router-dom";
+import setUser from '../redux/currentUser/currentUserAction';
 
 const ApplicationSettings = () => {
     const dispatch = useDispatch();
+    const navigate = useNavigate();
     const globalTheme = useSelector(
         state => state.globalThemeReducer.globalTheme
     );
+
+    const logOut = () => {
+        localStorage.removeItem("userInfo");
+        dispatch(setUser(['', '']))
+        navigate("/");
+    }
 
     return (
         <Box sx={{ p: 2, display: 'flex', justifyContent: 'right' }}>
@@ -17,7 +26,7 @@ const ApplicationSettings = () => {
             >
                 {globalTheme ? <BrightnessLow /> : <BrightnessHigh />}
             </IconButton>
-            <Button variant='outlined'>Log out</Button>
+            <Button onClick={logOut} variant='outlined'>Log out</Button>
         </Box>
     )
 }
