4aba1782f65f335e2c54201d1d0bb43a6fd0c41e
Nitesh <05niteshraj@gmail.com>
2023-05-20 01:57:10 +0530

    Solving Ansible Integration;

--- terraform/ec2.tf
+++ terraform/ec2.tf
@@ -1,7 +1,11 @@
+resource "aws_key_pair" "my_key_pair" {
+  key_name   = "my_key_pair"
+  public_key = file("my_key_pair.pub")
+}
 resource "aws_instance" "ec2_instance" {
   ami           = "ami-0c768662cc797cd75"
   instance_type = "t2.micro"
-  key_name      = var.SSH_KEY_NAME
+  key_name      = aws_key_pair.my_key_pair.key_name
   security_groups = [aws_security_group.instance_sg.name]
   tags = {
     Name = "twitsy-prod"
@@ -13,6 +17,6 @@ resource "aws_instance" "ec2_instance" {
 
   user_data = <<-EOF
               #!/bin/bash
-              echo "hello world"
+              echo "hello world" > test.txt
               EOF
 }
