1c5a32d740718dbb6ff336991ee92eb5a36ad866
Nitesh <05niteshraj@gmail.com>
2023-05-20 02:08:43 +0530

    Solving Ansible Integration;

--- terraform/ec2.tf
+++ terraform/ec2.tf
@@ -1,6 +1,6 @@
 resource "aws_key_pair" "my_key_pair" {
   key_name   = "my_key_pair"
-  public_key = file("my_key_pair.pub")
+  public_key = file("/home/runner/.ssh/my_key_pair.pub")
 }
 resource "aws_instance" "ec2_instance" {
   ami           = "ami-0c768662cc797cd75"
